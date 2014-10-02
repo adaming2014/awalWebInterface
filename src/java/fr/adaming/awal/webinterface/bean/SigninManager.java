1d7f9c8aa83ad1c72ba58349a9b9a47a0999602c
Antoine <git.alucas@gmail.com>
2014-10-02 10:58:57 +0200

    Perssist client

--- src/java/fr/adaming/awal/webinterface/bean/SigninManager.java
+++ src/java/fr/adaming/awal/webinterface/bean/SigninManager.java
@@ -5,11 +5,18 @@
  */
 package fr.adaming.awal.webinterface.bean;
 
+import fr.adaming.awal.controller.interfaces.IClientController;
+import fr.adaming.awal.entity.Client;
+import fr.adaming.awal.entity.User;
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -18,27 +25,58 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 @SessionScoped
 public class SigninManager implements Serializable {
-
+    
+    private static final String PAGE_INDEX = "index";
+    
     @ManagedProperty("#{authManager}")
     AuthManager authManager;
+    
+    ApplicationContext springContext;
 
     /**
      * Creates a new instance of SigninManager
      */
     public SigninManager() {
     }
-
-    public void signin() {
+    
+    @PostConstruct
+    public void init() {
+        springContext = new ClassPathXmlApplicationContext("spring-config.xml");
+    }
+    
+    public String signin() {
         FacesContext context = FacesContext.getCurrentInstance();
         SigninParameters signinParameters = context.getApplication().evaluateExpressionGet(context, "#{signinParameters}", SigninParameters.class);
-
-        System.out.println(signinParameters);
+        
+        IClientController clientController = (IClientController) springContext.getBean("clientController");
+        if (clientController == null) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Internal error", "Internal error"));
+            return null;
+        }
+        
+        User user = new User();
+        user.setFirstname(signinParameters.getFirstname());
+        user.setLastname(signinParameters.getLastname());
+        user.setMail(signinParameters.getEmail());
+        user.setPassword(signinParameters.getPassword());
+        
+        Client client = new Client();
+        client.setUser(user);
+        
+        if (!clientController.create(client)) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Internal error", "Internal error"));
+            return null;
+        }
+        
+        authManager.setUser(client);
+        
+        return PAGE_INDEX;
     }
-
+    
     public AuthManager getAuthManager() {
         return authManager;
     }
-
+    
     public void setAuthManager(AuthManager authManager) {
         this.authManager = authManager;
     }

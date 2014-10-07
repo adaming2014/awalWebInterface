6c66afcbdff2de700ea15ff541ee242d39bfe905
Antoine <git.alucas@gmail.com>
2014-10-07 17:10:19 +0200

    update signin page

--- src/java/fr/adaming/awal/webinterface/bean/manager/UserManager.java
+++ src/java/fr/adaming/awal/webinterface/bean/manager/UserManager.java
@@ -5,20 +5,15 @@
  */
 package fr.adaming.awal.webinterface.bean.manager;
 
-import fr.adaming.awal.webinterface.bean.form.UserParameters;
 import fr.adaming.awal.controller.interfaces.IClientController;
 import fr.adaming.awal.entity.Client;
 import fr.adaming.awal.entity.User;
+import fr.adaming.awal.webinterface.bean.form.UserParameters;
 import fr.adaming.awal.webinterface.util.FacesMessageUtil;
-import java.io.Serializable;
-import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -26,55 +21,42 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 @ManagedBean
 @SessionScoped
-public class UserManager implements Serializable {
-    
+public class UserManager extends GenericManager {
+
     private static final String PAGE_INDEX_REDIRECT = "index_redirect";
-    
+
     @ManagedProperty("#{authManager}")
     AuthManager authManager;
-    
-    ApplicationContext springContext;
 
-    /**
-     * Creates a new instance of SigninManager
-     */
-    public UserManager() {
-    }
-    
-    @PostConstruct
-    public void init() {
-        springContext = new ClassPathXmlApplicationContext("spring-config.xml");
-    }
-    
     public String signin() {
         FacesContext context = FacesContext.getCurrentInstance();
         UserParameters signinParameters = context.getApplication().evaluateExpressionGet(context, "#{userParameters}", UserParameters.class);
-        
+
         IClientController clientController = (IClientController) springContext.getBean("clientController");
-        
+
         User user = new User();
         user.setFirstname(signinParameters.getFirstname());
         user.setLastname(signinParameters.getLastname());
         user.setMail(signinParameters.getEmail());
         user.setPassword(signinParameters.getPassword());
-        
+
         Client client = new Client();
         client.setUser(user);
-        
+
         if (!clientController.create(client)) {
             context.addMessage(null, FacesMessageUtil.MESSAGE_DATABASE_ERROR);
             return null;
         }
-        
+
         authManager.setUser(client);
-        
+
         return PAGE_INDEX_REDIRECT;
     }
-    
+
     public AuthManager getAuthManager() {
         return authManager;
     }
-    
+
     public void setAuthManager(AuthManager authManager) {
         this.authManager = authManager;
     }

b59bd30b198cafdc2dc2f685b433a0938e9da0a7
Antoine <git.alucas@gmail.com>
2014-10-08 11:58:04 +0200

    Get bean in resetFields of ClientMAnager

--- src/java/fr/adaming/awal/webinterface/bean/manager/ClientManager.java
+++ src/java/fr/adaming/awal/webinterface/bean/manager/ClientManager.java
@@ -6,7 +6,11 @@
 package fr.adaming.awal.webinterface.bean.manager;
 
 import fr.adaming.awal.controller.interfaces.IClientController;
+import fr.adaming.awal.webinterface.bean.form.AddressParameters;
+import fr.adaming.awal.webinterface.bean.form.ClientParameters;
+import fr.adaming.awal.webinterface.bean.form.UserParameters;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
@@ -18,14 +22,27 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 public class ClientManager extends GenericManager {
 
+    @ManagedProperty("#{authManager}")
+    private AuthManager authManager;
+
     public void update() {
     }
 
     public void resetFields() {
         FacesContext context = FacesContext.getCurrentInstance();
+        UserParameters userParameters = getManagedBean(context, "userParameters", UserParameters.class);
+        ClientParameters clientParameters = getManagedBean(context, "clientParameters", ClientParameters.class);
+        AddressParameters addressParameters = getManagedBean(context, "addressParameters", AddressParameters.class);
 
         IClientController controller = (IClientController) springContext.getBean("clientController");
-
-        System.out.println(controller);
     }
+
+    public AuthManager getAuthManager() {
+        return authManager;
+    }
+
+    public void setAuthManager(AuthManager authManager) {
+        this.authManager = authManager;
+    }
+
 }

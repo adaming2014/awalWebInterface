46b4616c7e378aa01b6208386a25cfeb685014af
Antoine <git.alucas@gmail.com>
2014-10-08 11:34:50 +0200

    Make RepairerManager herit from GenericManager

--- src/java/fr/adaming/awal/webinterface/bean/manager/RepairerManager.java
+++ src/java/fr/adaming/awal/webinterface/bean/manager/RepairerManager.java
@@ -12,14 +12,10 @@ import fr.adaming.awal.entity.User;
 import fr.adaming.awal.webinterface.bean.form.RepairerParameters;
 import fr.adaming.awal.webinterface.bean.form.UserParameters;
 import fr.adaming.awal.webinterface.util.FacesMessageUtil;
-import java.io.Serializable;
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -27,25 +23,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 @ManagedBean
 @SessionScoped
-public class RepairerManager implements Serializable {
+public class RepairerManager extends GenericManager {
 
     private static final String PAGE_INDEX_REDIRECT = "index_redirect";
 
     @ManagedProperty("#{authManager}")
     AuthManager authManager;
-
-    ApplicationContext springContext;
-
-    /**
-     * Creates a new instance of ClientManager
-     */
-    public RepairerManager() {
-    }
-
-    @PostConstruct
-    public void init() {
-        springContext = new ClassPathXmlApplicationContext("spring-config.xml");
-    }
 
     public String update() {
         FacesContext context = FacesContext.getCurrentInstance();

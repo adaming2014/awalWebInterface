491b1eda826cc1c0528ed08ca1911d583bbf9058
Antoine <git.alucas@gmail.com>
2014-10-10 17:05:53 +0200

    Move actions from view to controller

--- src/java/fr/adaming/awal/webinterface/bean/manager/DeviceRepairManager.java
+++ src/java/fr/adaming/awal/webinterface/bean/manager/DeviceRepairManager.java
@@ -6,26 +6,16 @@
 package fr.adaming.awal.webinterface.bean.manager;
 
 import fr.adaming.awal.controller.interfaces.IClientController;
-import fr.adaming.awal.controller.interfaces.IDeviceInsuranceController;
 import fr.adaming.awal.controller.interfaces.IDeviceRepairController;
-import fr.adaming.awal.controller.interfaces.IRepairerController;
-import fr.adaming.awal.entity.Client;
 import fr.adaming.awal.entity.Device;
-import fr.adaming.awal.entity.Deviceinsurance;
 import fr.adaming.awal.entity.Devicerepair;
 import fr.adaming.awal.entity.Modelpackage;
-import fr.adaming.awal.entity.Repairer;
-import fr.adaming.awal.util.RepairerUtil;
-import java.io.Serializable;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import fr.adaming.awal.webinterface.util.FacesMessageUtil;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -33,97 +23,45 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 @ManagedBean
 @SessionScoped
-public class DeviceRepairManager implements Serializable {
+public class DeviceRepairManager extends GenericManager {
 
-    ApplicationContext springContext;
+    private static final String NAVIGATION_HOME_CLIENT = "pretty:home";
+
     private Modelpackage modelPackage;
     private Devicerepair deviceRepair;
     private Device device;
-    private static final String NAVIGATION_HOME_CLIENT = "pretty:home";
-    
+
+    @ManagedProperty("#{authManager}")
+    AuthManager authManager;
+
     /**
      * Creates a new instance of DeviceRepairManager
      */
     public DeviceRepairManager() {
-        springContext = new ClassPathXmlApplicationContext("spring-config.xml");
         deviceRepair = new Devicerepair();
     }
 
     public String add() {
-
         FacesContext context = FacesContext.getCurrentInstance();
-        AuthManager authManager = context.getApplication().evaluateExpressionGet(context, "#{authManager}", AuthManager.class);
-        Repairer repairer = null;
-        IDeviceRepairController deviceController = (IDeviceRepairController) springContext.getBean("deviceRepairController");
-        IRepairerController repairerController = (IRepairerController) springContext.getBean("repairerController");
-        IClientController clientController = (IClientController) springContext.getBean("clientController");
 
-        for (Devicerepair devicerepair : deviceController.getAll()) {
-            if ((devicerepair.getDevice().getIdDevice().equals(device.getIdDevice())) && (devicerepair.getModelpackage().equals(modelPackage))) {
-                return NAVIGATION_HOME_CLIENT;
-            }
-        }
-        for (Repairer repairer1 : repairerController.getAll()) {
+        IDeviceRepairController deviceRepairController = (IDeviceRepairController) springContext.getBean("deviceRepairController");
 
-            if (repairer1.getAvailable().equals(RepairerUtil.AVAILABLE)) {
-                if (repairer1.getFirm().getAddress().getPostcode().equals(clientController.getById(authManager.getClientId()).getAddress().getPostcode())) {
-                    repairer = repairer1;
+        Devicerepair devicerepair = new Devicerepair();
+        devicerepair.setDevice(device);
+        devicerepair.setModelpackage(modelPackage);
 
-                }
-            }
-        }
-        if(repairer == null){
-            for (Repairer repairer2 : repairerController.getAll()) {
-                if (repairer2.getAvailable().equals(RepairerUtil.AVAILABLE)) {
-                    repairer = repairer2;
-                }
-            }
-        }
-        
-        if(deviceAsInsurance(device,clientController.getById(authManager.getClientId()))==true){
-            deviceRepair.setPrice(0);
-        }else{
-            deviceRepair.setPrice(Integer.valueOf(modelPackage.getPrice()));
-        }
-        
-        deviceRepair.setRepairer(repairer);
-        deviceRepair.setDevice(device);
-        deviceRepair.setState("CREATE");
-        deviceRepair.setModelpackage(modelPackage);
-        deviceRepair.setDateCreation(new Date());
-
-
-        if (!deviceController.create(deviceRepair)) {
+        try {
+            deviceRepairController.createDeviceRepair(devicerepair);
+        } catch (IDeviceRepairController.PackageAlreadyPresentException ex) {
+            context.addMessage(null, FacesMessageUtil.MESSAGE_DEVICE_REPAIR_ALREADY_EXIST);
             return null;
         }
 
         return NAVIGATION_HOME_CLIENT;
     }
 
-    public boolean deviceAsInsurance(Device device1, Client client) {
-        boolean insurance = false;
-        IDeviceInsuranceController deviceInsuranceController = (IDeviceInsuranceController) springContext.getBean("deviceInsuranceController");
-        for (Deviceinsurance deviceinsurance : deviceInsuranceController.getDevicesInsuranceByClient(client)) {
-            if (deviceinsurance.getDevice().equals(device1)) {
-                Calendar calLastTime = Calendar.getInstance(); 
-                calLastTime.setTime(deviceinsurance.getBeginDate());               
-                int duration = deviceinsurance.getDeviceinsurancemodel().getDuration();
-                calLastTime.add(Calendar.MONTH, duration);
-                System.out.println(" calendar add duration : "+(calLastTime.get(Calendar.MONTH)+1)+"-"+calLastTime.get(Calendar.DATE)+"-"+calLastTime.get(Calendar.YEAR));
-                Calendar calTime = Calendar.getInstance(); 
-                if (calTime.compareTo(calLastTime)<=0) {
-                    insurance = true;
-                }
-            }
-        }
-        System.out.println("insurance : "+insurance);
-        return insurance;
-    }
-
     public List<Devicerepair> getDevicesRepairByClient() {
-        FacesContext context = FacesContext.getCurrentInstance();
         IClientController clientController = (IClientController) springContext.getBean("clientController");
-        AuthManager authManager = context.getApplication().evaluateExpressionGet(context, "#{authManager}", AuthManager.class);
         IDeviceRepairController deviceController = (IDeviceRepairController) springContext.getBean("deviceRepairController");
         return deviceController.getDevicesRepairByClient(clientController.getById(authManager.getClientId()));
     }
@@ -150,6 +88,14 @@ public class DeviceRepairManager implements Serializable {
 
     public void setDevice(Device device) {
         this.device = device;
+    }
+
+    public AuthManager getAuthManager() {
+        return authManager;
+    }
+
+    public void setAuthManager(AuthManager authManager) {
+        this.authManager = authManager;
     }
 
 }
--- src/java/fr/adaming/awal/webinterface/util/FacesMessageUtil.java
+++ src/java/fr/adaming/awal/webinterface/util/FacesMessageUtil.java
@@ -18,12 +18,11 @@ public class FacesMessageUtil {
     public static final FacesMessage MESSAGE_BEAN_NOT_FOUND = MESSAGE_INTERNAL_ERROR;
 
     public static final FacesMessage MESSAGE_FIRM_NOT_FOUND = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Firm : Firm not found", "Firm : Incorrect value of firm id");
+    public static final FacesMessage MESSAGE_DEVICE_NOT_CREATE = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Device : Device not create", "Device : Incorrect value of Device id");
+    public static final FacesMessage MESSAGE_MODEL_NOT_CREATE = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Model : Device not model", "Model : Incorrect value of Model id");
+    public static final FacesMessage MESSAGE_DEVICE_REPAIR_ALREADY_EXIST = new FacesMessage(FacesMessage.SEVERITY_ERROR, "DeviceRepair : DeviceRepair already exist", "DeviceRepair : DeviceRepair already exist");
 
     public static final FacesMessage INFO_FIRM_UPDATED = new FacesMessage(FacesMessage.SEVERITY_INFO, "Firm updated !", "The firm has been correctly updated.");
     public static final FacesMessage INFO_USER_UPDATED = new FacesMessage(FacesMessage.SEVERITY_INFO, "User updated !", "The user has been correctly updated.");
-
-    public static final FacesMessage MESSAGE_DEVICE_NOT_CREATE = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Device : Device not create", "Device : Incorrect value of Device id");
-    public static final FacesMessage MESSAGE_MODEL_NOT_CREATE = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Model : Device not model", "Model : Incorrect value of Model id");
-    
     public static final FacesMessage INFO_DEVICE_CREATE = new FacesMessage(FacesMessage.SEVERITY_INFO, "Device create !", "The Device has been correctly create.");
 }
